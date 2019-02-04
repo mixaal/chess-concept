a261f9dff4c6f1a9a8919301e1550ced1344a790
Michal Conos <michal.conos@oracle.com>
2019-02-04 12:41:49 +0100

    add board header file

---
+++ board.h
@@ -0,0 +1,30 @@
+#ifndef __MIX_BOARD_H__
+#define __MIX_BOARD_H__ 1
+
+
+#define ANSI_COLOR_RED     "\x1b[31m"
+#define ANSI_COLOR_GREEN   "\x1b[32m"
+#define ANSI_COLOR_YELLOW  "\x1b[33m"
+#define ANSI_COLOR_BLUE    "\x1b[34m"
+#define ANSI_COLOR_MAGENTA "\x1b[35m"
+#define ANSI_COLOR_CYAN    "\x1b[36m"
+#define ANSI_COLOR_RESET   "\x1b[0m"
+
+#define FULL_BLOCK_CHAR L'\u2588'
+#define SPACE_CHAR      L'\x20'
+#define NEW_LINE_CHAR   L'\x0a'
+#define W_KING_CHAR     L'\u2654'
+#define W_QUEEN_CHAR    L'\u2655'
+#define W_ROOK_CHAR     L'\u2656'
+#define W_BISHOP_CHAR   L'\u2657'
+#define W_KNIGHT_CHAR   L'\u2658'
+#define W_PAWN_CHAR     L'\u2659'
+#define B_KING_CHAR     L'\u265a'
+#define B_QUEEN_CHAR    L'\u265b'
+#define B_ROOK_CHAR     L'\u265c'
+#define B_BISHOP_CHAR   L'\u265d'
+#define B_KNIGHT_CHAR   L'\u265e'
+#define B_PAWN_CHAR     L'\u265f'
+
+
+#endif
